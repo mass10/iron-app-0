e40a798ce2d7ddbae844d8388573112c27838a6e
Irisawa Masaru <mass10.github@gmail.com>
2020-05-08 23:26:32 +0900

    wip

--- src/application/mylib/util.rs
+++ src/application/mylib/util.rs
@@ -1,6 +1,7 @@
 pub struct Util;
 
 impl Util {
+	/// システムのタイムスタンプを文字列で返します。
 	pub fn get_current_timestamp() -> String {
 		let date = chrono::Local::now();
 		return format!("{}", date.format("%Y-%m-%d %H:%M:%S%.3f"));
